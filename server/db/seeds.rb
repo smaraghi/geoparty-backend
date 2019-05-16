66ca81f2ccd9b71d1b8d9b4c3cc5b05470a3815e
github username <github email address>
2019-05-15 23:56:27 -0400

    finished/fixed seeds file

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -7,39 +7,49 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 
+Country.destroy_all
+
 req = open("#{Rails.root}/db/factbook.json")
 body = req.read
-json = JSON.parse(body) 
-
-names = json['countries'].map{ |k, v| v['data']['name'] }
-
+json = JSON.parse(body)
 
 json['countries'].each do |k, v|
-  name = v['data']['name']
-  profile = v['data']['introduction']['background']
-  location = v['data']['geography']['location']
-  continent = v['data']['geography']['map_references']
-  area = v['data']['geography']['area']['total']['value']
-  area_rank = v['data']['geography']['area']['global_rank']
-  comparison = v['data']['geography']['area']['comparative']
+  data = v['data']
+  geo = data['geography']
+  people = data['people']
+
+  name = data['name']
+  profile = data['introduction']['background']
   
-  
-  border_countries = ""
-  if !v['data']['geography']['land_boundaries']['border_countries'].nil?
-    v['data']['geography']['land_boundaries']['border_countries'].each do |c|
-      border_countries += c['country'] + " - " 
-    end
-    border_countries = border_countries[0, border_countries.length - 3]
-  else 
-    border_countries = 'none'
+  location = geo['location']
+  continent = geo['map_references']
+  area = geo['area']['total']['value']
+  area_rank = geo['area']['global_rank']
+  comparison = geo['area']['comparative']
+  climate = geo['climate']
+  terrain = geo['terrain']
+
+  begin
+    border_countries = geo['land_boundaries']['border_countries'].map{|i| i['country']}.join('-')
+  rescue
+    border_countries = "None"
   end
-  
 
-  climate = v['data']['geography']['climate']
-  terrain = v['data']['geography']['terrain']
-  # lowest_point = v['data']['geography']['elevation']['lowest_point']['name']
-  # highest_point = v['data']['geography']['elevation']['highest_point']['name']
-  
+  begin
+    lowest_point = geo['elevation']['lowest_point']['name']
+    highest_point = geo['elevation']['highest_point']['name']
+    natural_resources = geo['natural_resources']['resources'].join('-')
+    population = people['population']['total']
+    population_rank = people['population']['global_rank']
+    nationality = people['nationality']['noun']
+    languages = people['languages']['language'].map{|i| i['name']}.join('-')
+    religions = people['religions']['religion'].map{|i| i['name']}.join('-')
+    major_cities = people['major_urban_areas']['places'].map{|i| i['place']}
+    capital = people['major_urban_areas']['places'].select{|i| i['is_capital']}
+  rescue
+    puts name
+    next
+  end
 
-  Country.create(name: name, profile: profile, location: location, continent: continent, area: area, area_rank: area_rank, comparison: comparison, border_countries: border_countries, climate: climate, terrain: terrain)
+  Country.create(name: name, profile: profile, location: location, continent: continent, area: area, area_rank: area_rank, comparison: comparison, border_countries: border_countries, climate: climate, terrain: terrain, lowest_point: lowest_point, highest_point: highest_point, natural_resources: natural_resources, population: population, population_rank: population_rank, nationality: nationality, languages: languages, religions: religions, major_cities: major_cities, capital: capital)
 end
