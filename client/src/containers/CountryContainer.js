aae68c7682ad445628a74fad81cd5c1677f82ce1
Serven Maraghi <maraghiserven@gmail.com>
2019-05-20 12:41:45 -0400

    Add semantic grid to country profile

--- client/src/containers/CountryContainer.js
+++ client/src/containers/CountryContainer.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import DisplayContainer from './DisplayContainer';
 import CountrySelector from '../components/CountrySelector';
+import { Grid } from 'semantic-ui-react'
 
 const CountryContainer = () => {
   const [activeCountry, setActiveCountry] = useState('Spain')
@@ -10,11 +11,14 @@ const CountryContainer = () => {
   }
 
   return(
-    <div>
-    {console.log(activeCountry)}
-      <CountrySelector handleCountry={handleCountry}/>
-      <DisplayContainer activeCountry={activeCountry}/>
-    </div>
+    <Grid>
+      <Grid.Column width={3}  id='party'>
+        <CountrySelector handleCountry={handleCountry}/>
+      </Grid.Column>
+      <Grid.Column width={13}>
+        <DisplayContainer activeCountry={activeCountry}/>
+      </Grid.Column>
+    </Grid>
   )
 }
 
