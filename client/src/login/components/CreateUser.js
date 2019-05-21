1a458bcb716b47872137ac0e0bdbd9527f596a4d
github username <github email address>
2019-05-21 18:44:54 -0400

    Add create user functionality, Add bio and avatar to backend

--- client/src/login/components/CreateUser.js
+++ client/src/login/components/CreateUser.js
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom'
+import { Form, Segment, Header, Button } from 'semantic-ui-react'
 
 const CreateUser = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [bio, setBio] = useState('')
+  const [redirect, setRedirect] = useState(false)
+
+  const handleSubmit = () => {
+    fetch('http://localhost:3000/users', {
+      method: "POST",
+      headers:{"Content-Type":"application/json", "Accept": "application/json"},
+      body:JSON.stringify({
+    		username: username,
+    		password: password,
+        bio: bio
+    	})
+    })
+    .then(res => res.json())
+    .then(user => {
+      if (user.id) {
+        setRedirect(true)
+      } else {
+        window.alert('invalid username or password')
+      }
+    })
+  }
+
+  const renderRedirect = () => {
+    if (redirect) {
+      return <Redirect to='/login' />
+    }
+  }
+
   return (
-    <div>yo</div>
+    <Segment>
+      {renderRedirect()}
+      <Header as='h2'>Create Account party</Header>
+      <Form onSubmit={(e) => {
+        e.preventDefault()
+        handleSubmit()
+      }}>
+        <Form.Input
+          label="username"
+          placeholder="username"
+          name="username"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+        <Form.Input
+          label="password"
+          placeholder="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <Form.TextArea
+          label='bio'
+          name='bio'
+          placeholder='Tell us more about yourself...'
+          onChange={(e) => setBio(e.target.value)}
+          value={bio}
+        />
+        <Button type='submit'>Submit</Button>
+      </Form>
+    </Segment>
   )
 }
 
--- client/src/profile/containers/Bio.js
+++ client/src/profile/containers/Bio.js
@@ -1,16 +1,21 @@
 import React, { useContext } from 'react'
 import shopContext from '../../context/shop-context'
 import { Card, Image } from 'semantic-ui-react'
+import { isEmpty } from 'lodash'
 
 const Bio = () => {
   const context = useContext(shopContext)
 
   return (
     <Card id='profile-bio'>
+      {isEmpty(context.user) ?
       <Image src={require("../../images/star.png")} alt="party"/>
+      :
+      <Image src={require(`../../images/${context.user.avatar}`)} alt="party"/>
+      }
       <Card.Content>
         <Card.Header>{context.user.username}</Card.Header>
-        <Card.Description>eat, sleep, party, repeat</Card.Description>  
+        <Card.Description>{context.user.bio}</Card.Description>
       </Card.Content>
     </Card>
   )
