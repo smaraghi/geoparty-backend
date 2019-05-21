5ef86f4a000885beb5ec8abb11ed2d1738962b04
Serven Maraghi <maraghiserven@gmail.com>
2019-05-21 16:02:50 -0400

    Edit to loggin file structure

---
+++ client/src/login/containers/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginForm from './LoginForm';
+import CreateUser from '../components/CreateUser';
+
+const Login = () => {
+  return(
+    <div></div>
+  )
+}
+
+export default Login
