70e78ca26e4ee21c230711c83a1abe82be4c196c
github username <github email address>
2019-05-21 20:18:11 -0400

    Add login redirect to profile page

--- client/src/app/MainContainer.js
+++ client/src/app/MainContainer.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React, { useContext } from 'react'
+import shopContext from '../context/shop-context'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Country from '../country/containers/Country'
 import Trivia from '../trivia/containers/Trivia'
 import Profile from '../profile/containers/Profile';
 import Login from '../login/containers/Login'
 import CreateUser from '../login/components/CreateUser'
+import { isEmpty } from 'lodash'
 
-const MainContainer = () => {
+const MainContainer = props => {
+  const context = useContext(shopContext)
+  
   return(
     <div>
       <Switch>
         <Route exact path='/country' component={Country}/>
         <Route exact path='/trivia' component={Trivia} />
         <Route exact path='/profile' component={Profile} />
-        <Route exact path='/login' component={Login} />
+        <Route exact path="/login" render = { (props) => {
+          return isEmpty(context.user) ? <Login/> : <Redirect to="/profile"/>
+        }} />
         <Route exact path='/create_account' component={CreateUser} />
       </Switch>
     </div>
