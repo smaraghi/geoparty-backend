6714720266fdbe971d094fff9f9fccd913422fc2
github username <github email address>
2019-05-21 20:32:23 -0400

    Refactor navbar to conditionally render based on logged in status

--- client/src/app/Navbar.js
+++ client/src/app/Navbar.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import shopContext from '../context/shop-context'
 import { Menu, Icon } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import { isEmpty } from 'lodash'
 
 const Navbar = () => {
+  const context = useContext(shopContext)
+
   return (
+    !isEmpty(context.user) ?
     <Menu >
       <Link to='/about'>
         <Menu.Item header>
@@ -19,9 +24,30 @@ const Navbar = () => {
       <Link to='/trivia'>
         <Menu.Item name='Trivia' />
       </Link>
-      
+
       <Link to='/profile'>
         <Menu.Item name='Profile' />
+      </Link>
+
+      <Link to='/login' onClick={context.logoutUser}>
+        <Menu.Item name='Logout' />
+      </Link>
+    </Menu>
+    :
+    <Menu>
+      <Link to='/about'>
+        <Menu.Item header>
+          GeoParty &nbsp;
+          <Icon name='globe' />
+        </Menu.Item>
+      </Link>
+
+      <Link to='/country'>
+        <Menu.Item name='Country' />
+      </Link>
+
+      <Link to='/trivia'>
+        <Menu.Item name='Trivia' />
       </Link>
 
       <Link to='/login'>
@@ -31,4 +57,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar 
+export default Navbar
