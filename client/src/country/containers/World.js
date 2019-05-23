6febc084b9e53432928feaa52df51ecafc508229
github username <github email address>
2019-05-23 14:17:33 -0400

    Add styling to world component to match countries

--- client/src/country/containers/World.js
+++ client/src/country/containers/World.js
@@ -4,20 +4,20 @@ import { Header, Segment } from 'semantic-ui-react'
 const World = () => {
   return(
     <div>
-      <Header as='h1' >The World</Header>
-      <Segment>
+      <Header id='country-header' as='h1' >The World</Header>
+      <Segment id='country-summary'>
         The surface of the earth is approximately 70.9% water and 29.1% land. The former portion is divided into large bodies termed oceans. The World Factbook recognizes and describes five oceans, which are in decreasing order of size: the Pacific Ocean, Atlantic Ocean, Indian Ocean, Southern Ocean, and Arctic Ocean. Because of their immense size, the Pacific and Atlantic Oceans are generally divided at the equator into the North and South Pacific Oceans and the North and South Atlantic Oceans, thus creating seven major water bodies - the so-called "Seven Seas." <br/><br/>
-        
+
         Some 97.5% of the earth's water is saltwater. Of the 2.5% that is fresh, about two-thirds is frozen mostly locked up in the Antarctic ice sheets and mountain glaciers worldwide. If all the surface ice on earth fully melted, the sea level would rise about 70 m.<br/><br/>
-        
+
         Earth's land portion is generally divided into several, large, discrete landmasses termed continents. Depending on the convention used, the number of continents can vary from five to seven. The most common classification recognizes seven, which are (from largest to smallest): Asia, Africa, North America, South America, Antarctica, Europe, and Australia. Asia and Europe are sometimes lumped together into a Eurasian continent resulting in six continents. Alternatively, North and South America are sometimes grouped as simply the Americas, resulting in a continent total of six (or five, if the Eurasia designation is used).<br/><br/>
-        
+
         North America is commonly understood to include the island of Greenland, the isles of the Caribbean, and to extend south all the way to the Isthmus of Panama. The easternmost extent of Europe is generally defined as being the Ural Mountains and the Ural River; on the southeast the Caspian Sea; and on the south the Caucasus Mountains, the Black Sea, and the Mediterranean. Portions of five countries - Azerbaijan, Georgia, Kazakhstan, Russia, and Turkey - fall within both Europe and Asia, but in every instance the larger section is in Asia. These countries are considered part of both continents. Armenia and Cyprus, which lie completely in Western Asia, are geopolitically European countries.<br/><br/>
-        
+
         Asia usually incorporates all the islands of the Philippines, Malaysia, and Indonesia. The islands of the Pacific are often lumped with Australia into a "land mass" termed Oceania or Australasia. Africa's northeast extremity is frequently delimited at the Isthmus of Suez, but for geopolitical purposes, the Egyptian Sinai Peninsula is often included as part of Africa.<br/><br/>
-        
+
         Although the above groupings are the most common, different continental dispositions are recognized or taught in certain parts of the world, with some arrangements more heavily based on cultural spheres rather than physical geographic considerations.<br/><br/>
-        
+
         Based on the seven-continent model, and grouping islands with adjacent continents, Africa has the most countries with 54. Europe contains 49 countries and Asia 48, but these two continents share five countries: Azerbaijan, Georgia, Kazakhstan, Russia, and Turkey. North America consists of 23 sovereign states, Oceania has 14, and South America 12.<br/>
       </Segment>
     </div>
