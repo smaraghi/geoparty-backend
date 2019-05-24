cf478c661ca783c3bacddf3f841d97f596b78b0b
Serven Maraghi <maraghiserven@gmail.com>
2019-05-24 12:56:28 -0400

    Add background color and image

--- server/app/controllers/questions_controller.rb
+++ server/app/controllers/questions_controller.rb
@@ -15,6 +15,7 @@ class QuestionsController < ApplicationController
     # body.gsub!('&quot\;', '"')
     body.gsub!('&iacute;', 'í')
     body.gsub!('&oacute;', 'ó')
+    #&atilde:
 
     json = JSON.parse(body)
     json['results'] = json['results'].shuffle
