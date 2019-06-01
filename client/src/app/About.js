ea86a4097462af5614f75f0dd03b542f686fe220
Serven Maraghi <sarvin@Servens-MacBook-Pro.local>
2019-06-01 14:08:43 -0400

    Add styling to homepage

--- client/src/app/About.js
+++ client/src/app/About.js
@@ -4,11 +4,12 @@ import { Segment, Header, Image } from 'semantic-ui-react'
 const About = () => {
   return(
     <div id='about-container'>
-			<Header id='about-header' as='h1'>About</Header>
       <Segment id='about-blurb'>
 				<p>GeoParty is a lightweight learning tool for users of all ages! Discover new and interesting facts about countries spanning the globe. Challenge yourself with an assortment of trivia questions and keep track of your progress. Compete with friends to answer the most correct questions and earn the greatest number of stars!</p>
 			</Segment>
-      <Image id='about-map'  src={require('../images/world-map.jpg')} alt='world map' />
+      <div id='about-map-container'>
+        <Image id='about-map'  src={require('../images/world-map.jpg')} alt='world map' />
+      </div>
     </div>
 	)
 }
