3eceabb11ec4a5c3e964348b66112f4d37c66234
Serven Maraghi <maraghiserven@gmail.com>
2019-06-04 14:37:38 -0400

    Move auth helper methods to auth_controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,4 +1,3 @@
-
 class AuthController < ApplicationController
 
   def create
@@ -17,6 +16,20 @@ class AuthController < ApplicationController
         authenticated: false
       }, status: :unauthorized
     end
+  end
+  
+  private
+
+  def secret_key
+    Rails.application.credentials.geoparty[:secret_key]
+  end
+
+  def encode(payload)
+    JWT.encode(payload, secret_key, 'HS256')
+  end
+
+  def decode(token)
+    JWT.decode(token, secret_key, true, {algorithm: 'HS256'})[0]
   end
 
 end
