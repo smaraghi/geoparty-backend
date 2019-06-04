a54eda259027bb508fa97ab3217a7e7879c20b96
Serven Maraghi <maraghiserven@gmail.com>
2019-06-04 14:51:20 -0400

    Modify application controller and auth controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::API
 
+  private
+
+  def secret_key
+    Rails.application.credentials.geoparty[:secret_key]
+  end
+
+  def encode(payload)
+    JWT.encode(payload, secret_key, 'HS256')
+  end
+
+  def decode(token)
+    JWT.decode(token, secret_key, true, {algorithm: 'HS256'})[0]
+  end
+
 end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -18,18 +18,5 @@ class AuthController < ApplicationController
     end
   end
   
-  private
-
-  def secret_key
-    Rails.application.credentials.geoparty[:secret_key]
-  end
-
-  def encode(payload)
-    JWT.encode(payload, secret_key, 'HS256')
-  end
-
-  def decode(token)
-    JWT.decode(token, secret_key, true, {algorithm: 'HS256'})[0]
-  end
 
 end
